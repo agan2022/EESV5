a6539e44b9a68c85754054c93958340c0ac3c1fe
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-02 23:57:48 +0330

    create Proposal Services

---
+++ EESV2.DAL/Services/Interfaces/IProposalService.cs
@@ -0,0 +1,13 @@
+﻿using EESV2.DAL.ViewModels.Prroposal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EESV2.DAL.Services.Interfaces
+{
+    public interface IProposalService
+    {
+    }
+}
---
+++ EESV2.DAL/Services/ProposalService.cs
@@ -0,0 +1,20 @@
+﻿using EESV2.DAL.Services.Interfaces;
+using EESV2.DAL.ViewModels.Prroposal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EESV2.DAL.Services
+{
+    public class ProposalService: IProposalService
+    {
+        private IUnitOfWork _uw;
+
+        public ProposalService(IUnitOfWork uw)
+        {
+            _uw = uw;
+        }
+    }
+}
