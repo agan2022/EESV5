b7de8d6fbfc3e215c71b62584ce7ed138035f30a
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-02 23:57:25 +0330

    Create ParticipantProposalViewModel.cs

---
+++ EESV2.DAL/ViewModels/Prroposal/ParticipantProposalViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EESV2.DAL.ViewModels.Prroposal
+{
+    public class ParticipantProposalViewModel
+    {
+        public int ID { get; set; }
+
+        public int StatusID { get; set; }
+
+        public string Date { get; set; }
+
+        public string SubjectPr { get; set; }
+
+        public string Status { get; set; }
+    }
+}
