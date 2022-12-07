ec5ae8c8850f68d34ceb675261f3a06c5606d0a6
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-07 22:06:55 +0330

    modify rank calculator

--- EESV2.DAL/Repository/RankCalculator.cs
+++ EESV2.DAL/Repository/RankCalculator.cs
@@ -157,11 +157,14 @@ namespace EESV2.DAL.Repository
                                 .Include(u => u.Proposals)
                                 .ThenInclude(p => p.Imparts)
                                 .ThenInclude(p => p.Reports)
-                                .Include(p=> p.ProposalsThatHelped)
-                                .ThenInclude(p=> p.Proposal)
                                 .ToList();
+            //get user for participant proposal
+            var users2 = _context.Users
+                .Include(u => u.ProposalsThatHelped);
 
+            int score = 0;
             List<UserRankViewModel> userRankViewModels = new List<UserRankViewModel>();
+            //proposal
             foreach (User user in users)
             {
                 int q1 = user.Proposals.Where(p => p.StatusID != 3).Count();//تعداد پیشنهادات رد نشده کاربر
@@ -172,6 +175,56 @@ namespace EESV2.DAL.Repository
                 int q6 = user.Proposals.Where(p => p.StatusID == 2 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).Count();//تعداد پیشنهادات که تصویب کمی شده اند ولی هنوز به واحد مجری ارسل نشده اند
                 int q7 = user.Proposals.Where(p => p.StatusID == 2 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).Count();//تعداد پیشنهادات که تصویب کیفی شده اند ولی هنوز به واحد مجری ارسل نشده اند
 
+                
+
+
+                score = (q1 > 30 ? 30 : q1) + (q6 * 30 + q7 * 10) ;
+
+                foreach (Proposal p in p1)
+                {
+                    score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
+                }
+
+
+
+                foreach (Proposal p in p2)
+                {
+                    score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
+                }
+
+
+
+                foreach (Proposal p in p3)
+                {
+                    score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
+                }
+
+
+
+                foreach (Proposal p in p4)
+                {
+                    score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
+                }
+
+
+
+                UserRankViewModel temp = new UserRankViewModel()
+                {
+                    ID=user.ID,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    PersonnelCode = user.Username,
+                    Office = user.Office.Name,
+                    ProposalCount = user.Proposals.Count,
+                    Score = score,
+                    Rank = 1
+                };
+                userRankViewModels.Add(temp);
+            }
+
+            //participant proposal
+            foreach (var user in users2)
+            {
                 #region Participant Proposal
 
                 int qqp1 = user.ProposalsThatHelped
@@ -203,23 +256,13 @@ namespace EESV2.DAL.Repository
 
                 #endregion
 
+                score = (qqp1 > 30 ? 30 : qqp1) + (qp6 * 30 + qp7 * 10);
 
-                int score = (q1 > 30 ? 30 : q1) + (q6 * 30 + q7 * 10) + (qqp1 > 30 ? 30 : qqp1) + (qp6 * 30 + qp7 * 10);
-
-                foreach (Proposal p in p1)
-                {
-                    score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
-                }
 
                 //Participant
                 foreach (Proposal p in qp1)
                 {
                     score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
-                }
-
-                foreach (Proposal p in p2)
-                {
-                    score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
                 }
 
                 //Participant
@@ -228,20 +271,10 @@ namespace EESV2.DAL.Repository
                     score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
                 }
 
-                foreach (Proposal p in p3)
-                {
-                    score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
-                }
-
                 //Participant
                 foreach (Proposal p in qp3)
                 {
                     score += 60 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
-                }
-
-                foreach (Proposal p in p4)
-                {
-                    score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
                 }
 
                 //Participant
@@ -249,10 +282,11 @@ namespace EESV2.DAL.Repository
                 {
                     score += 20 * _darsadPishraftCalculator.CalculateDarsadPishraft(p.ID) / 100;
                 }
+                
 
                 UserRankViewModel temp = new UserRankViewModel()
                 {
-                    ID=user.ID,
+                    ID = user.ID,
                     FirstName = user.FirstName,
                     LastName = user.LastName,
                     PersonnelCode = user.Username,
@@ -261,9 +295,14 @@ namespace EESV2.DAL.Repository
                     Score = score,
                     Rank = 1
                 };
+
+                userRankViewModels.Remove(temp);
                 userRankViewModels.Add(temp);
             }
-            userRankViewModels=userRankViewModels.OrderByDescending(v => v.Score).ToList();
+
+
+
+            userRankViewModels =userRankViewModels.OrderByDescending(v => v.Score).ToList();
             for (int i = 1; i < userRankViewModels.Count; i++)
             {
                 if (userRankViewModels[i].Score == userRankViewModels[i - 1].Score)
