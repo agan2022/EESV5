996df45ca8dcf475831102b672ddc276068a84f2
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-15 14:25:43 +0330

    Modify Views and Controller For Changed

--- EESV2/Controllers/TopMemberReportController.cs
+++ EESV2/Controllers/TopMemberReportController.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EESV2.DAL.Services.Interfaces;
 
 namespace EESV2.Controllers
 {
@@ -17,10 +18,14 @@ namespace EESV2.Controllers
     {
         private readonly IUnitOfWork _uw;
         private readonly IProposalRepository _proposalRepository;
-        public TopMemberReportController(IUnitOfWork uw,IProposalRepository proposalRepository)
+
+        private IRankService _rankService;
+
+        public TopMemberReportController(IUnitOfWork uw, IProposalRepository proposalRepository, IRankService rankService)
         {
             _uw = uw;
             _proposalRepository = proposalRepository;
+            _rankService = rankService;
         }
 
         [HttpGet]
@@ -33,132 +38,12 @@ namespace EESV2.Controllers
         {
             if (ModelState.IsValid)
             {
-                List<User> users = _uw.UserRepository.Get(u=>model.OfficeIDs.Contains((int)u.OfficeID),include:s=>s
-                                                            .Include(u=>u.Office)
-                                                            .Include(p=> p.ProposalsThatHelped.Where(p => EF.Functions.Like(p.Proposal.Date, (model.getYear() != 0 ? model.getYear() : "%") + "/" + (model.getMonth() != 0 ? model.getMonth().ToString("0#") : "%") + "/%")))
-                                                            .ThenInclude(p=> p.Proposal)
-                                                            .ThenInclude(p=> p.Referrals)
-                                                            .Include(p => p.ProposalsThatHelped.Where(p => EF.Functions.Like(p.Proposal.Date, (model.getYear() != 0 ? model.getYear() : "%") + "/" + (model.getMonth() != 0 ? model.getMonth().ToString("0#") : "%") + "/%")))
-                                                            .ThenInclude(p => p.Proposal)
-                                                            .ThenInclude(p=> p.Imparts)
-                                                            .ThenInclude(p=> p.Reports)
-                                                            .Include(u=>u.Proposals.Where(p =>EF.Functions.Like(p.Date, (model.getYear() != 0 ? model.getYear() : "%") + "/" + (model.getMonth() != 0 ? model.getMonth().ToString("0#") : "%") + "/%")))
-                                                            .ThenInclude(p=>p.Referrals)
-                                                            .Include(u => u.Proposals.Where(p => EF.Functions.Like(p.Date, (model.getYear() != 0 ? model.getYear() : "%") + "/" + (model.getMonth() != 0 ? model.getMonth().ToString("0#") : "%") + "/%")))
-                                                            .ThenInclude(p => p.Imparts)
-                                                            .ThenInclude(p => p.Reports))
-                                                            .ToList();
 
-                model.userRankViewModels = new List<UserRankViewModel>();
-                foreach (User user in users)
-                {
-                    int q1 = user.Proposals.Where(p => p.StatusID != 3).Count();//تعداد پیشنهادات رد نشده کاربر
-                    List<Proposal> p1 = user.Proposals.Where(p => p.StatusID == 9 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).ToList() ;// پیشنهاداتی که به واحد مجری ارسال شده اند و تصویب شده کمی هستند
-                    List<Proposal> p2 = user.Proposals.Where(p => p.StatusID == 9 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).ToList();// پیشنهاداتی که به واحد مجری ارسال شده اند و تصویب شده کیفی هستند
-                    List<Proposal> p3 = user.Proposals.Where(p => p.StatusID == 5 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).ToList();// پیشنهاداتی که اجرا شده کمی هستند
-                    List<Proposal> p4 = user.Proposals.Where(p => p.StatusID == 5 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).ToList();// پیشنهاداتی که اجرا شده کیفی هستند
-                    int q6 = user.Proposals.Where(p => p.StatusID == 2 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).Count();//تعداد پیشنهادات که تصویب کمی شده اند ولی هنوز به واحد مجری ارسل نشده اند
-                    int q7 = user.Proposals.Where(p => p.StatusID == 2 && p.Referrals.Count > 0 && p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).Count();//تعداد پیشنهادات که تصویب کیفی شده اند ولی هنوز به واحد مجری ارسل نشده اند
+                var userRank = _rankService.TopMembersRankCalculation(model);
 
-                    #region Participant Proposal
+                model.userRankViewModels=userRank;
 
-                    int qqp1 = user.ProposalsThatHelped
-                       .Select(p => p.Proposal).Count(p => p.StatusID != 3);//تعداد پیشنهادات رد نشده گروهی کاربر
-
-                    var qp1 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 9 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).ToList();//پیشنهادات گروهی که به واحد مجری ارسال شده اند و تصویب شده کمی هستند
-
-                    var qp2 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 9 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).ToList();//پیشنهادات گروهی که به واحد مجری ارسال شده اند و تصویب شده کیفی هستند
-
-                    var qp3 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 5 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).ToList();// پیشنهادات گروهی ای که اجرا شده کمی هستند
-
-                    var qp4 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 5 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).ToList();//پیشنهادات گروهی که اجرا شده کیفی هستند
-
-                    int qp6 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 2 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 1).Count();//تعداد پیشنهادات گروهی که تصویب کمی شده اند ولی هنوز به واحد مجری ارسال نشده اند
-
-                    int qp7 = user.ProposalsThatHelped.Select(p => p.Proposal).Where(p =>
-                        p.StatusID == 2 && p.Referrals.Count > 0 &&
-                        p.Referrals.OrderBy(r => r.ID).LastOrDefault().EvaluationTypeID == 2).Count();//تعداد پیشنهادات گروهی ای که تصویب کیفی شده اند ولی هنوز به واحد مجری ارسال نشده اند
-
-                    #endregion
-
-                    int score = (q1 > 30 ? 30 : q1)+(q6 * 30 + q7 * 10) + (qqp1 > 30 ? 30 : qqp1) + (qp6 * 30 + qp7 * 10);
-                    foreach (Proposal p in p1)
-                    {
-                        score += 60 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-                    foreach (Proposal p in p2)
-                    {
-                        score += 20 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-                    foreach (Proposal p in p3)
-                    {
-                        score += 60 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-                    foreach (Proposal p in p4)
-                    {
-                        score += 20 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-
-                    //Participant
-                    foreach (Proposal p in qp1)
-                    {
-                        score += 60 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-
-                    //Participant
-                    foreach (Proposal p in qp2)
-                    {
-                        score += 20 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-
-                    //Participant
-                    foreach (Proposal p in qp3)
-                    {
-                        score += 60 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-
-                    //Participant
-                    foreach (Proposal p in qp4)
-                    {
-                        score += 20 * _proposalRepository.CalculateDarsadPishraft(p) / 100;
-                    }
-
-                    UserRankViewModel temp = new UserRankViewModel()
-                    {
-                        FirstName = user.FirstName,
-                        LastName = user.LastName,
-                        PersonnelCode = user.Username,
-                        Office = user.Office.Name,
-                        ProposalCount = user.Proposals.Count,
-                        Score = score,
-                        Rank = 1,
-                        ParticipantProposal = user.ProposalsThatHelped.Count
-                    };
-                    model.userRankViewModels.Add(temp);
-                }
-                model.userRankViewModels = model.userRankViewModels.OrderByDescending(v => v.Score).ToList();
-                for (int i = 1; i < model.userRankViewModels.Count; i++)
-                {
-                    if (model.userRankViewModels[i].Score== model.userRankViewModels[i-1].Score)
-                    {
-                        model.userRankViewModels[i].Rank = model.userRankViewModels[i - 1].Rank;
-                    }
-                    else
-                    {
-                        model.userRankViewModels[i].Rank = model.userRankViewModels[i - 1].Rank+1;
-                    }
-                }
-                ViewData["viewModel"] = model.userRankViewModels;
+                ViewData["viewModel"] = userRank;
             }
             return View(model);
         }
