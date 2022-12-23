67556a1b86c72ea46b9c4cbd14fbd738b718fba6
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-24 01:06:05 +0330

    add Persian Date Time Libery

---
+++ EESV2.DAL/Convertor/DateConvertor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using MD.PersianDateTime.Core;
+
+namespace EESV2.DAL.Convertor
+{
+    public static class DateConvertor
+    {
+        public static DateTime ConvertPersianDateToChristianDate(this string value)
+        {
+            PersianDateTime pd=PersianDateTime.Parse(value);
+
+            return pd;
+        }
+    }
+}
