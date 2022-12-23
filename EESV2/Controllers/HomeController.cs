31bfa815c3f68bdfd6843234befd5555a0e81240
agan2022 <86554998+agan2022@users.noreply.github.com>
2022-12-24 01:07:05 +0330

    Filter Score and Users by Date In View

--- EESV2/Controllers/HomeController.cs
+++ EESV2/Controllers/HomeController.cs
@@ -5,15 +5,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EESV2.DAL.Services.Interfaces;
+using MD.PersianDateTime.Core;
 
 namespace EESV2.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IRankService _rankService;
+
+        public HomeController(IRankService rankService)
         {
-            return View();
+            _rankService = rankService;
+        }
+
+        public IActionResult Index(int Date)
+        {
+            if (Date == 0)
+            { 
+                PersianDateTime pr=new PersianDateTime(DateTime.Now);
+                Date=pr.Year;
+            }
+
+            var model = _rankService.RankCalculation(null, Date, 0);
+
+            return View(model);
         }
         public IActionResult AdvanceDashboard(AdvanceDashboardViewModel model)
         {
